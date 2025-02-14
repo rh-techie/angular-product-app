567a26b6beae27404ea337ce2ad0cf84420bb2a7
rh-techie <robbiehui.techie@gmail.com>
2025-02-13 19:05:57 -0800

    added a component for product detail

---
+++ src/app/components/product-card/product-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product } from '../../models/product';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.scss']
+})
+export class ProductCardComponent {
+  @Input() product!: Product;
+  @Output() viewDetails = new EventEmitter<void>();
+
+  onViewDetails() {
+    this.viewDetails.emit();
+  }
+}
